f5bc259f36578d475c6585c68da531d4f239dd0b
pabasara-mahindapala <pabasaramahindapala@gmail.com>
2024-11-26 16:08:53 +0530

    Display user profile

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   if (req.isAuthenticated()) {
-    res.render("index", { title: "Express" });
+    res.render("index", { title: "Express", user: req.user });
   } else {
     res.render("login", { title: "Express" });
   }
