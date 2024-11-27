7f7695d33e44ae02a8692c76bd417fd2bf6f030d
pabasara-mahindapala <pabasaramahindapala@gmail.com>
2024-11-27 10:07:35 +0530

    Securing routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
+var ensureLogIn = require("connect-ensure-login").ensureLoggedIn;
 var router = express.Router();
 
+var ensureLoggedIn = ensureLogIn();
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", ensureLoggedIn, function (req, res, next) {
+  res.send("This is a protected resource");
 });
 
 module.exports = router;
